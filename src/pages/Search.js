f15659aaa59519d5dfea9b6d13b57e54e52b504a
Matheus Vital Diniz de Souza <matheus@localhost.localdomain>
2022-03-07 19:24:18 -0300

    Add fiveth requirement

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,10 +3,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const min = 2;
+    let result = true;
+    if (target.value.length >= min) result = false;
+    this.setState({
+      name: target.value,
+      disabled: result,
+    });
+  }
+
   render() {
+    const { name, disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            name={ name }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            disabled={ disabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
